fcce06e2da360152dd463e41c78ade35362c5b09
Tiphani Campbell <tiphani.campbell@gmail.com>
2022-03-20 18:58:06 -0500

    New changes

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET KEY'] = "my-totally-random-secret-key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wfmbtzdmojfapi:e060475cbbe70fb9a0393b9e39becb0be9fe08d3afdbd33a4a8566b9799de939@ec2-3-230-238-86.compute-1.amazonaws.com:5432/d1utr9l2rhd7pc'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bqhaqhykbajcii:020cb17e6784c2d259a5fae71f197808c6a631c1aeab23cd29fcb685c2352d14@ec2-34-224-226-38.compute-1.amazonaws.com:5432/dbvda02i2cs6q6'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 app.config.from_object(__name__)
