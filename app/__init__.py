35416c39f472d344d27cb82977f5b5b3c00ffcad
Tiphani Campbell <tiphani.campbell@gmail.com>
2022-03-20 19:06:09 -0500

    added secret key

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SECRET KEY'] = "my-totally-random-secret-key"
+app.config['SECRET_KEY'] = "my-totally-random-secret-key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bqhaqhykbajcii:020cb17e6784c2d259a5fae71f197808c6a631c1aeab23cd29fcb685c2352d14@ec2-34-224-226-38.compute-1.amazonaws.com:5432/dbvda02i2cs6q6'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
